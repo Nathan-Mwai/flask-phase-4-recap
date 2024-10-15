d2b26aa9a36d55d5e73d35fac993b49c5519d6d4
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-15 18:59:53 +0300

    Added final adjustments for today

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 # from flask import Flask
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api
 from models import *
@@ -25,15 +25,35 @@ api = Api(app)
 def index():
     return '<h1>I am running</h1>'
 
-@app.route('/users')
-def getting_users():
-    users = [user.to_dict() for user in User.query.all()]
-    return  make_response(users, 200)
+@app.route('/users', methods=['GET','POST'])
+def users():
+    if request.method == "GET":
+        users = [user.to_dict() for user in User.query.all()]
+        return  make_response(users, 200)
+    elif request.method== 'POST':
+        data = request.get_json()
+        user = User(
+            username=data['username'],
+            email=data['email']
+        )
+        db.session.add(user)
+        db.session.commit()
+        
+        return make_response(user.to_dict(), 204)
+@app.route('/users/<int:id>', methods=['DELETE', 'PATCH', 'GET'])
+def users_by_id(id):
+    if request.method == "GET":
+        user = User.query.filter(User.id==id).first()
+        return (user.to_dict(),200)
+    # elif request.method == "POST":
+        
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET','POST'])
 def get_posts():
     posts = [post.to_dict() for post in Post.query.all()]
     return make_response(posts, 200)
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
